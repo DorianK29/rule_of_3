95c26dad7cb7dc1a50395a6a853aa7b6498061a6
DorianK29 <doriank29tehc@gmail.com>
2024-01-18 12:51:42 +0900

    added config + corrected spelling issues :)

--- __init__.py
+++ __init__.py
@@ -7,10 +7,28 @@ from aqt.browser import Browser
 from aqt.qt import *
 from aqt.utils import tooltip
 
-# Constant for the action name
-ACTION_NAME = "Rule of 3's"
+# Read config
+config = mw.addonManager.getConfig(__name__)
 
-# Function to check the "Rule of 3's" for a card
+# Load config values
+enableLearn = config['enable-learn']
+learnConsecutive = config['learn-consecutive']
+enableLearnTag = config['enable-learn-tag']
+learnTag = config['learn-tag']
+enableLearnSuspend = config['enable-learn-suspend']
+
+enableForget = config['enable-forget']
+forgetConsecutive = config['forget-consecutive']
+enableForgetTag = config['enable-forget-tag']
+forgetTag = config['forget-tag']
+enableForgetSuspend = config['enable-forget-suspend']
+
+ignoreSuspendedCards = config['ignore-suspended-cards']
+
+# Constant for the action name
+ACTION_NAME = "Rule of 3s"
+
+# Function to check the "Rule of 3s" for a card
 def checkCard(*args):
     if len(args) == 1:
         card = args[0]
@@ -20,59 +38,77 @@ def checkCard(*args):
     else:
         raise ValueError("Invalid number of arguments")
 
+    if ignoreSuspendedCards:
+        if card.queue == -1: # if card is suspended
+            return 0
+
     # Retrieve the list of reviews for the card from the database
     review_list = mw.col.db.all(f"SELECT ease, type FROM revlog WHERE cid = '{card.id}' ORDER BY id ASC ")
 
     n = len(review_list)
 
-    review_count = 0
-    consecutive_corrects = 0
+    # Check for wrong answers
+    if enableForget:
+        if n >= forgetConsecutive:  # Check if a card has more than n previous reviews
+            # Extract ease (rating) values from the last n reviews
+            last_n_reviews = [review[0] for review in review_list[-forgetConsecutive:]]
 
-    if n >= 3:  # Check if a card has more than 3 previous reviews
-        # Extract ease (rating) values from the last 3 reviews
-        last_three_reviews = [review[0] for review in review_list[-3:]]
+            # Check if all the last n reviews are ratings 1 or 2
+            if all(rating in (1, 2) for rating in last_n_reviews):
+                # Set the card properties and add a tag
+                if enableForgetSuspend:
+                    card.queue = -1
+                    card.flush()
+                if enableForgetTag:
+                    card.note().tags.append(forgetTag)
+                    card.note().flush()
+                if ease != None:
+                    tooltip(f"Card marked: {forgetConsecutive} consecutive wrongs.")
+                return 1
 
-        # Check if all the last 3 reviews are ratings 1 or 2
-        if all(rating in (1, 2) for rating in last_three_reviews):
-            # Set the card properties and add a tag
-            card.queue = -1
-            card.flush()
-            card.note().tags.append("Relearn")
-            card.note().flush()
-            if ease != None:
-                tooltip("Card suspended: 3 consecutive forgottens.")
-            return
+    # Check for correct answers
+    if enableLearn:
+        review_count = 0
+        consecutive_corrects = 0
+        for review in review_list:
+            rating = review[0]
+            revType = review[1]
 
-    for review in review_list:
-        rating = review[0]
-        revType = review[1]
+            if rating in (3, 4):
+                consecutive_corrects += 1
+                if revType != 0: # if the card is out of its learning stage
+                    review_count += 1
+            else:
+                consecutive_corrects = 0
+                review_count = 0
 
-        if rating in (3, 4):
-            consecutive_corrects += 1
-            if revType != 0:
-                review_count += 1
-        else:
-            consecutive_corrects = 0
-            review_count = 0
+            if consecutive_corrects >= learnConsecutive and review_count >= learnConsecutive/2:
+                # Set the card properties
+                if enableLearnSuspend:
+                    card.queue = -1
+                    card.flush()
+                if enableLearnTag:
+                    card.note().tags.append(learnTag)
+                    card.note().flush()
+                if ease != None:
+                    tooltip(f"Card marked: {learnConsecutive} consecutive corrects.")
+                return 2
 
-        if consecutive_corrects >= 3 and review_count >= 2:
-            # Set the card properties
-            card.queue = -1
-            card.flush()
-            if ease != None:
-                tooltip("Card suspended: 3 consecutive corrects.")
-            return
+    return 0
 
-# Function to perform the "Rule of 3's" check on selected cards in bulk
+# Function to perform the "Rule of 3s" check on selected cards in bulk
 def bulk_check_rule_of_3(nids: Sequence):
     mw.checkpoint(ACTION_NAME)
     mw.progress.start()
 
+    answers = [0, 0, 0]
+
+
     for nid in nids:
         card = mw.col.get_card(nid)
-        checkCard(card)
+        answers[checkCard(card)] += 1
 
-    tooltip(f"Checked {len(nids)} notes.")
+    tooltip(f"Checked {answers[0]} notes.\nMarked {answers[1]} new forgotten cards and {answers[2]} new learnt cards.")
 
     mw.progress.finish()
     mw.reset()
---
+++ config.md
@@ -0,0 +1,14 @@
+* `enable-learn`: enable/disable checking for consecutive correct answers  (default: `true`)
+* `learn-consecutive`: number of consecutive correct answers after which a card is considered leant (default: `3`)
+* `enable-learn-tag`: enable/disable adding a tag to learnt cards (default: `false`)
+* `learn-tag`: name of the tag (default: `learnt`)
+* `enable-learn-suspend`: enable/disable suspending a learnt card (default: `true`)
+* `enable-forget`: enable/disable checking for consecutive forgotten answers  (default: `true`)
+* `forget-consecutive`: number of consecutive wrong answers after which a card is considered forgotten (default: `3`)
+* `enable-forget-tag`: enable/disable adding a tag to forgotten cards (default: `true`)
+* `forget-tag`: name of the tag (default: `relearn`)
+* `enable-forget-suspend`: enable/disable suspending a forgotten card (default: `true`)
+* `ignore-suspended-cards`: enable/disable checking of suspended cards (default: `true`) 
+
+
+Some changes will not take effect until you restart anki.
